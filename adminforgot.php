c365f3b074f2beb745b6c84125cbfcb3d5a7ebc6
Ho2 <howard_bb@hotmail.com>
2020-08-25 22:07:46 +0800

    created admin forgot password

--- adminforgot.php
+++ adminforgot.php
@@ -1,10 +1,25 @@
 <?php
-include_once('_admin.function.php');
-include_once('admincheck.php');
-if (havesession()) {
-    header("location:admindashboard.php"); //if the cookie or session is empty, go to login
+if (!isset($_GET['err'])) {
+    $email_err = '';
+} else {
+    $err = $_GET['err'];
+
+    if ($err == 0) {
+        $email_err = "Wrong Email Struture!";
+    } elseif ($err == 1) {
+        $email_err = "Email not existed!";
+    }
 }
 
+
+include_once('_admin.function.php');
+if (havesession()) {
+    header("location:admindashboard.php"); //if cookie/session is valid, go to dashboard
+}
+if (isset($_POST["submit"])) //If all the field is filled
+{
+    sendforgotemail($_POST['email']);
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,92 +28,58 @@ if (havesession()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Restaurant | Login</title>
+    <title>My Restaurant | Forgot Password</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="./fontawesome-free-5.14.0-web/css/all.css">
-    <script src="./fontawesome-free-5.14.0-web/js/all.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./style/adminlogin.css">
-
-    <!--JS for the show and hide passsword-->
-    <script>
-        function showpass() {
-            var x = document.getElementById("password");
-            if (x.type === "password") {
-                x.type = "text";
-            } else {
-                x.type = "password";
-            }
-        }
-    </script>
+    <link rel="stylesheet" href="./style/register.css">
 </head>
 
 <body>
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <nav class="navbar navbar-expand navbar-light bg-light sticky-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php"><img src="img/res_logo.png" height=50>My Restaurant</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-
-
-                </ul>
-            </div>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="login.php" class="nav-link active">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="register.php" class="nav-link">Sign Up</a>
+                </li>
+            </ul>
         </div>
     </nav>
 
-    <!-- Login Form -->
+    <!-- Email Verification -->
     <div class="modal-dialog text-center">
-        <div class="col-md-12 main-section">
+        <div class="col-12 main-section">
             <div class="modal-content">
-                <div class="col-md-12 user-img">
+                <div class="col-12 user-img">
                     <img src="img/admin/adminlogo.png" width="130" height="auto">
                 </div>
-                <hr class="light">
+                <hr>
 
                 <div class="row">
-                    <div class="col-md-7">
-                        <h2>Admin Login</h2>
+                    <div class="col-12">
+                        <h2>Forgot Password</h2>
                     </div>
-                    <div class="col-md-5 forgot"><a href="adminforgot.php">Forgot Password?</a></div>
                 </div>
 
-                <form action="adminlogin.php" method="POST" class="col-md-12">
-                    <div class="formctrl form-group">
-                        <input type="text" name="username" class="form-control" placeholder="UserID or Email Address" required>
-                        <span class="errmsg"><?php echo $username_err; ?></span>
+                <form action="adminforgot.php" method="POST" class="col-12">
+                    <div class="form-group" id="email">
+                        <input name="email" type="email" class="form-control" placeholder="Eg. name@example.com" required>
+                        <span><?php echo $email_err; ?></span>
                     </div>
-                    <div class="formctrl form-group pass">
-                        <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-                        <span class="errmsg"><?php echo $password_err; ?></span>
-                    </div>
-                    <div class="col-md-12 checkbox">
-                        <input type="checkbox" onclick="showpass()">
-                        <label>Show Password</label>
-
-                    </div>
-                    <div class="col-md-12 checkbox">
-                        <input type="checkbox" name="remember">
-                        <label>Remember Me</label>
-                    </div>
-
-                    <button class="btn col-md-12" name="submit" type="submit">Login</button>
-
-
+                    <button class="btn col-12" type="submit" name="submit">Continue</button>
                 </form>
             </div>
         </div>
     </div>
-
 
     <!-- Footer -->
     <footer>
