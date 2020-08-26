6dbf9b1d31a73606f85a1e58e2a403db09f82b08
Ho2 <howard_bb@hotmail.com>
2020-08-26 17:27:54 +0800

    updated sql, minor change to dashboard

--- admindashboard.php
+++ admindashboard.php
@@ -117,6 +117,104 @@ checksession();
                 <h3 class="display-5">Admin <?php echo $_SESSION['admin_row']['name'] ?></h3>
             </div>
         </div>
+
+        <!-- Details display-->
+        <div class="container detail-box">
+            <div class="row">
+
+                <div class="col-lg-4 col-xs-12 text-center">
+                    <div class="box">
+                        <i class="fa fa-behance fa-3x" aria-hidden="true"></i>
+                        <div class="box-title">
+                            <h3>Behance</h3>
+                        </div>
+                        <div class="box-text">
+                            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                        </div>
+                        <div class="box-btn">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-xs-12  text-center">
+                    <div class="box">
+                        <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                        <div class="box-title">
+                            <h3>Twitter</h3>
+                        </div>
+                        <div class="box-text">
+                            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                        </div>
+                        <div class="box-btn">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-xs-12 text-center">
+                    <div class="box">
+                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                        <div class="box-title">
+                            <h3>Facebook</h3>
+                        </div>
+                        <div class="box-text">
+                            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                        </div>
+                        <div class="box-btn">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-xs-12 text-center">
+                    <div class="box">
+                        <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
+                        <div class="box-title">
+                            <h3>Pinterest</h3>
+                        </div>
+                        <div class="box-text">
+                            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                        </div>
+                        <div class="box-btn">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-xs-12 text-center">
+                    <div class="box">
+                        <i class="fa fa-google-plus fa-3x" aria-hidden="true"></i>
+                        <div class="box-title">
+                            <h3>Google</h3>
+                        </div>
+                        <div class="box-text">
+                            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                        </div>
+                        <div class="box-btn">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-xs-12 text-center">
+                    <div class="box">
+                        <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                        <div class="box-title">
+                            <h3>Github</h3>
+                        </div>
+                        <div class="box-text">
+                            <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                        </div>
+                        <div class="box-btn">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 
 
