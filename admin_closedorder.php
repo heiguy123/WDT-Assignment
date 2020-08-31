79acfaf729a15d48f614a80a67eded39d2892014
Ho2 <howard_bb@hotmail.com>
2020-08-31 22:27:13 +0800

    minor progress on view menu

--- admin_closedorder.php
+++ admin_closedorder.php
@@ -195,7 +195,7 @@ if (isset($_POST['order_id'])) {
             </table>
         </div>
 
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -213,7 +213,7 @@ if (isset($_POST['order_id'])) {
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- end of body -->
     </div>
 
